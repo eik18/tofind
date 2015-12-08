51e41506306919305be6d9281155ea7e4d4331b2
Eric Kelson <ekelson@constantcontact.com>
2015-12-08 09:26:00 -0500

    Removed a few comments

--- torfindmit.py
+++ torfindmit.py
@@ -1,10 +1,12 @@
-url='https://torstatus.blutmagie.de/query_export.php/Tor_query_EXPORT.csv'
-header_valid=[u'Router Name', u'Country Code', u'Bandwidth (KB/s)', u'Uptime (Hours)', u'IP Address', u'Hostname', u'ORPort', u'DirPort', u'Flag - Authority', u'Flag - Exit', u'Flag - Fast', u'Flag - Guard', u'Flag - Named', u'Flag - Stable', u'Flag - Running', u'Flag - Valid', u'Flag - V2Dir', u'Platform', u'Flag - Hibernating', u'Flag - Bad Exit', u'FirstSeen', u'ASName', u'ASNumber', u'ConsensusBandwidth', u'OrAddress']
-dname='mit.edu'
+#!/usr/bin/which python
 
 import requests
 import re
-from netaddr import  IPAddress
+from netaddr import IPAddress
+
+url='https://torstatus.blutmagie.de/query_export.php/Tor_query_EXPORT.csv'
+header_valid=[u'Router Name', u'Country Code', u'Bandwidth (KB/s)', u'Uptime (Hours)', u'IP Address', u'Hostname', u'ORPort', u'DirPort', u'Flag - Authority', u'Flag - Exit', u'Flag - Fast', u'Flag - Guard', u'Flag - Named', u'Flag - Stable', u'Flag - Running', u'Flag - Valid', u'Flag - V2Dir', u'Platform', u'Flag - Hibernating', u'Flag - Bad Exit', u'FirstSeen', u'ASName', u'ASNumber', u'ConsensusBandwidth', u'OrAddress']
+dname='mit.edu'
 
 class findtor():
     """findtor class"""
@@ -43,7 +45,6 @@ class findtor():
         self.iplist.sort()
         print "{0};{1};{2};{3};{4}".format(self.headers[0],self.headers[4],self.headers[5],self.headers[6],self.headers[7])
         for item in self.iplist:
-            #print "{0}\n".format(self.results[item])
             temparray=self.results[item].split(',')
             print "{0};{1};{2};{3};{4}".format(temparray[0],temparray[4],temparray[5],temparray[6],temparray[7])
 
